ffe4435ff35f67bfe0413acf04afe759ebb31c85
IMRAN <imran789924@gmail.com>
2020-04-15 18:47:53 +0600

    Accuracy improved and face is marked with box

--- RTFR_comb.py
+++ RTFR_comb.py
@@ -252,8 +252,10 @@ classifier.fit(trainX, trainy)
 
 
 
-
-
+#playground
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(trainX, trainy)
 
 
 
@@ -268,6 +270,37 @@ import cv2
 import numpy as np
 import os 
 import matplotlib.pyplot as plt
+from math import sin, cos, radians
+
+#for rotating face
+face = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+
+settings = {
+    'scaleFactor': 1.3, 
+    'minNeighbors': 3, 
+    'minSize': (50, 50), 
+    'flags': cv2.CASCADE_FIND_BIGGEST_OBJECT|cv2.CASCADE_DO_ROUGH_SEARCH
+}
+
+def rotate_image(image, angle):
+    if angle == 0: return image
+    height, width = image.shape[:2]
+    rot_mat = cv2.getRotationMatrix2D((width/2, height/2), angle, 0.9)
+    result = cv2.warpAffine(image, rot_mat, (width, height), flags=cv2.INTER_LINEAR)
+    return result
+
+def rotate_point(pos, img, angle):
+    if angle == 0: return pos
+    x = pos[0] - img.shape[1]*0.4
+    y = pos[1] - img.shape[0]*0.4
+    newx = x*cos(radians(angle)) + y*sin(radians(angle)) + img.shape[1]*0.4
+    newy = -x*sin(radians(angle)) + y*cos(radians(angle)) + img.shape[0]*0.4
+    return int(newx), int(newy), pos[2], pos[3]
+
+
+
+
+
 
 
 cascadePath = "haarcascade_frontalface_default.xml"
@@ -287,9 +320,15 @@ minH = 0.1*cam.get(4)
 while True:
     print('taking image....')
     ret, img_main =cam.read()
-    gray = cv2.cvtColor(img_main,cv2.COLOR_BGR2GRAY)
     
+    for angle in [0, -25, 25]:
+        rimg = rotate_image(img_main, angle)
+        detected = face.detectMultiScale(rimg, **settings)
+        if len(detected):
+            detected = [rotate_point(detected[-1], rimg, -angle)]
+            break
     
+    '''gray = cv2.cvtColor(img_main, cv2.COLOR_BGR2GRAY)
     
     faces = faceCascade.detectMultiScale( 
         gray,
@@ -298,18 +337,21 @@ while True:
         minSize = (int(minW), int(minH)),
        )
     for(x,y,w,h) in faces:
-        cv2.rectangle(img_main, (x,y), (x+w,y+h), (0,255,0), 2)
-    
+        cv2.rectangle(img_main, (x,y), (x+w,y+h), (0,255,0), 2)'''
+    for x, y, w, h in detected[-1:]:
+        cv2.rectangle(img_main, (x, y), (x+w, y+h), (255,0,0), 2)
+        
+        img = rimg[y:y+h,x:x+w]
     
 
-        img = gray[y:y+h,x:x+w]
-        img_rgb = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
+        #img = gray[y:y+h,x:x+w]
+        #img_rgb = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
         
-        res = cv2.resize(img_rgb, dsize=(160, 160), interpolation=cv2.INTER_CUBIC)
+        res = cv2.resize(img, dsize=(160, 160), interpolation=cv2.INTER_CUBIC)
         
-        #testX = Image.fromarray(img_rgb.astype('uint8'), 'RGB')
-        #testX = extract_face_rt(image)
-        
+        #res = Image.fromarray(img_main.astype('uint8'), 'RGB')
+        #res = extract_face_rt(res)
+    
         if(len(res) < 1):
             continue
         
@@ -322,9 +364,14 @@ while True:
         
         id = classifier.predict(testX)
         
+        prob = classifier.predict_proba(testX)
+        
+        confidence = prob[0][id]
+        
         id_text = out_encoder.inverse_transform(id)
         
         print(id_text)
+        print(confidence)
         
         cv2.putText(
                     img_main, 
@@ -335,7 +382,16 @@ while True:
                     (255,255,255), 
                     2
                    )
-         
+        cv2.putText(
+                    img_main, 
+                    str(confidence), 
+                    (x+5,y+h-5), 
+                    font, 
+                    1, 
+                    (255,255,0), 
+                    1
+                   )
+        
     
     cv2.imshow('camera',img_main) 
     k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
@@ -347,3 +403,56 @@ cam.release()
 cv2.destroyAllWindows()
 
 
+
+
+'''
+
+
+import cv2
+from math import sin, cos, radians
+camera =  cv2.VideoCapture(0)
+face = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+
+settings = {
+    'scaleFactor': 1.3, 
+    'minNeighbors': 3, 
+    'minSize': (50, 50), 
+    'flags': cv2.CASCADE_FIND_BIGGEST_OBJECT|cv2.CASCADE_DO_ROUGH_SEARCH
+}
+
+def rotate_image(image, angle):
+    if angle == 0: return image
+    height, width = image.shape[:2]
+    rot_mat = cv2.getRotationMatrix2D((width/2, height/2), angle, 0.9)
+    result = cv2.warpAffine(image, rot_mat, (width, height), flags=cv2.INTER_LINEAR)
+    return result
+
+def rotate_point(pos, img, angle):
+    if angle == 0: return pos
+    x = pos[0] - img.shape[1]*0.4
+    y = pos[1] - img.shape[0]*0.4
+    newx = x*cos(radians(angle)) + y*sin(radians(angle)) + img.shape[1]*0.4
+    newy = -x*sin(radians(angle)) + y*cos(radians(angle)) + img.shape[0]*0.4
+    return int(newx), int(newy), pos[2], pos[3]
+
+while True:
+    ret, img = camera.read()
+
+    for angle in [0, -25, 25]:
+        rimg = rotate_image(img, angle)
+        detected = face.detectMultiScale(rimg, **settings)
+        if len(detected):
+            detected = [rotate_point(detected[-1], img, -angle)]
+            break
+
+    # Make a copy as we don't want to draw on the original image:
+    for x, y, w, h in detected[-1:]:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255,0,0), 2)
+
+    cv2.imshow('facedetect', img)
+
+    if cv2.waitKey(5) != -1:
+        break
+
+cv2.destroyWindow("facedetect")
+'''
